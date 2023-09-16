7b5e622f41f2098428be2f8040ec7a139ada4f5a
JuniperTea <realityisasimulation@gmail.com>
2023-09-16 09:25:34 -0700

    another rebuild

--- server/index.js
+++ server/index.js
@@ -22,6 +22,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../app/build")));
 
+app.use("/", usersRouter);
 app.use("/login", usersRouter); //ladning = source page
 app.use("/users", authenticate, usersListRouter);
 app.use("/books", authenticate, booksRouter);
